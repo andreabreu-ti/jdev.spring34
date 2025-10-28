584a12b2f404a1dfd1d1d23301172480762557ce
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2025-10-28 17:14:13 -0300

    Configurações e criação da entidade

---
+++ jdev.spring34/src/main/java/br/com/jdev/spring34/entity/Pessoa.java
@@ -0,0 +1,26 @@
+package br.com.jdev.spring34.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	private String sobrenome;
+
+	
+}
